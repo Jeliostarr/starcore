e1fe2e285dec83b5889de15c30dc151ce4dd3868
Jelio Starr Dev <jeliostarrdev@gmail.com>
2025-09-25 13:40:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const __path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+
+let server = require('./qr');
+let code = require('./pair');
+
+// Increase event listeners if needed
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/qr', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+// Middleware should be set up BEFORE routes
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Static file serving (if you have CSS, JS, images)
+app.use(express.static(__path));
+
+// Routes
+app.use('/qr', server);
+app.use('/code', code);
+
+app.use('/pair', (req, res) => {
+    res.sendFile(__path + '/pair.html');
+});
+
+app.use('/', (req, res) => {
+    res.sendFile(__path + '/main.html');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).send('Page not found');
+});
+
 app.listen(PORT, () => {
     console.log(`
 Don't Forget To Give Star
+Server running on http://localhost:${PORT}`);
+});
 
- Server running on http://localhost:` + PORT)
-})
-
-module.exports = app
+module.exports = app;
